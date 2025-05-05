123c6b412c3e7afc359511a0ba36b4caf0c64a37
axel <natandreik11@gmail.com>
2025-05-05 00:10:59 -0400

    ticket

--- ejercicio3.py
+++ ejercicio3.py
@@ -3,4 +3,6 @@
 
 #Solicita duración de la canción en minutos (float)
 
-float duracion de la cancion en minutos
+duracion_cancion_minutos = input("se solicita la duracion de su cancion en minutos:")
+genero_musical = 'cual es tu genero musical'
+print = (genero_musical)
--- ejercicio4.py
+++ ejercicio4.py
@@ -1,8 +1,3 @@
-#desarrolla una calculadora para una tienda de ropa en linea que:
-
-#Defina constantes para diferentes categorías de descuentos, 
-# DESCUENTO_ESTUDIANTE = 0.15, DESCUENTO_CLIENTE_FRECUENTE = 0.10, etc.
-
 DESCUENTO_ESTUDIANTE = 0.15
 DESCUENTO_CLIENTE_FRECUENTE = 0.10
 
